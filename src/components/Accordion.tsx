091ad506ffa4f1b132b7c43d95dab2e45d4dcf48
Zoki <zm2666@g.rit.edu>
2021-06-10 19:11:46 +0200

    change color onClick

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -10,13 +10,15 @@ export const Accordion: React.FC<AccordionProps> = ({
   title,
   subtitle
 }: AccordionProps): ReactElement => {
-  const initialStyling = "pb-6 pr-18";
-  const content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, neque quam sequi praesentium'
-  + 'magnam quidem aliquam, quo a earum repudiandae, temporibus perferendis obcaecati aliquid dolorem iusto impedit nulla possimus iure.';
+  const initialStyling = "pb-6 pr-20";
+  const content = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum dolore facere recusandae magni voluptatem dolorem,'
+  + 'eligendi aperiam eum, quis debitis explicabo incidunt minima cumque velit unde blanditiis quasi iusto doloremque.';
   const [text, setText] = useState('');
+  const [colorChange, setColorChange] = useState(false);
 
 function showContent(event: any){
     setText(content);
+    setColorChange(true);
 }
 
   const border = [
@@ -29,11 +31,11 @@ function showContent(event: any){
   ]
 
   return (
-    <div className={border.join(" ")} onClick={showContent}>
-      <div className="text-1xl pt-4">
+    <div className={border.join(" ")} onClick={showContent} style={{borderLeftColor: colorChange ? 'rgb(179, 188, 190)' : 'rgb(9, 153, 179)'}}>
+      <div className="text-1xl pt-4" style={{color: colorChange ? 'rgb(9, 153, 179)' : 'black'}}>
         {subtitle}
       </div>
-      <div className="text-3xl pb-4 font-semibold">
+      <div className="text-3xl pb-2 font-semibold">
         {title}
       </div>
       <div className={initialStyling}>
